f859d46f7b536c64a9888445c350e8329c64562a
Luis Miguel Ribeiro lemes da Silva <108533442+Luestq@users.noreply.github.com>
2024-03-21 14:30:32 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-03-22T00:00:00");
+let tempoAtual = new Date();
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 console.log(botoes);
 
-for (let i=0; i< botoes.length; i++){
+for (let i=0; i < botoes.length; i++){
     botoes[i].onclick=function(){
-        botoes[i].classList.add("ativo");
-
+        
         for(let j=0; j<botoes.length; j++){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
-    botoes[i].classList.add("ativo");
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+       
     }
 }
